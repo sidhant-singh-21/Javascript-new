8f181fb2a70ce9ffbfa6d6b18079ab6bdcabfe87
Singh Siddhant <158172975+sidhant-singh-21@users.noreply.github.com>
2025-01-01 12:13:49 +0000

    rename file

---
+++ 01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1245
+let accountEmail = "singhsiddhant3762@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ahmedabad"
+
+// accountId = 1234 // it cannot change because it is a constant Variables
+
+accountEmail = "Singhsiddhant1805@gmail.com"
+accountPassword = "2341"
+accountCity = "Bihar"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
