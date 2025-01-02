fdd9f62ebb449b1d36d03d2133e9e494e293fa5a
Singh Siddhant <158172975+sidhant-singh-21@users.noreply.github.com>
2025-01-02 03:42:44 +0000

    final conversion

--- 03_coversionoperation.js
+++ 03_coversionoperation.js
@@ -23,8 +23,59 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+// *********************** Operations **************************** 
+
+let value = 33
+
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " sidhant"
+
+let str3 = str1 + str2
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100 
+// gameCounter++
+++gameCounter
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
